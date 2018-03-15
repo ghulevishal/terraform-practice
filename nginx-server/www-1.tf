e76fd17dad7822e77bc053240552e39694b7e740
vishalcloudyuga <vishal@cloudyuga.guru>
2018-03-15 13:48:12 +0530

    nginx

---
+++ nginx-server/www-1.tf
@@ -0,0 +1,26 @@
+resource "digitalocean_droplet" "www-1" {
+    name = "nginx-server"
+    image = "ubuntu-16-04-x64"
+    size = "512mb"
+    region = "ams3"
+    ipv6 = true
+    private_networking = false
+    ssh_keys = [
+      "${var.ssh_fingerprint}"
+    ]
+    connection {
+      user = "root"
+      type = "ssh"
+      private_key = "${file(var.pvt_key)}"
+      timeout = "2m"
+      }
+    provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx"
+    ]
+  }
+}
+
